e5f6f919741401e4e91919fe3cb2a8ef97e80739
Nigel Gott <nigel.gott@gmail.com>
2012-11-18 16:13:49 +0000

    Got basic storage / retrieval working.

--- js/meals.js
+++ js/meals.js
@@ -1,13 +1,29 @@
 meals = (function(){
-    // Gets all meals from a certain timespan.
-    var get = function(from, to){
-    };
+    var storage = chrome.storage.local;
 
-    var store = function(){
+    var store = function(meals){
+        storage.get('meals', function(result){
+            stored_meals = result['meals'];
+            if(stored_meals === undefined)
+                stored_meals = []
+            else
+                stored_meals = JSON.parse(stored_meals);
+
+            stored_meals.push.apply(stored_meals, meals);
+            storage.set({'meals': JSON.stringify(stored_meals)}, function(){
+                console.log('stored meals');
+            });
+        });
     };
 
     return {
         populate_with_last_year : function(){
+            store(['all Bran Breakfast', 'Turkey Pita']);
+        },
+        get_meals : function(callback){
+            storage.get('meals', function(result){
+                callback(result['meals']);
+            });
         }
     };
 })();
--- js/options.js
+++ js/options.js
@@ -3,5 +3,15 @@ $(document).ready(function() {
         meals.populate_with_last_year();
         return false;
     });
+
+    $('#get_meals_link').click(function(){
+        meals.get_meals(function(meals){
+            meals = JSON.parse(meals);
+            for(var i = 0; i < meals.length; i++){
+                $('#meals').append('<p>'+meals[i]+'</p>');
+            }
+        });
+        return false;
+    });
 });
 
