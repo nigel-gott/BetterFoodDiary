05dad3bb9f24631f9f360aac13510b69b3154ae2
Nigel Gott <nigel.gott@gmail.com>
2012-11-18 13:49:38 +0000

    Reset most of project to basics.
    Realised that the direction i was going in was silly, we should not be
    building anything on the printable_report page other than using it at
    installation to get mass meal data. Now everything will be built purely
    into the food diary (which actually makes sense...).

--- scripts/browser_action_event.js
+++
@@ -1,4 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({url: 'http://www.myfitnesspal.com/reports/printable_diary?bfd'});
-});
-
--- scripts/normal_printable_diary.js
+++
@@ -1,1 +0,0 @@
-$('#content').prepend('<a id="bfd_link" href="' + window.location.pathname + '?bfd">Open BFD</a>');
