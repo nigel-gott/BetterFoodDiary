a60f845d3e51e4e4e32145b3be3a314538f142a2
Nigel Gott <nigel.gott@gmail.com>
2012-11-21 22:06:31 +0000

    Added some comments.

--- js/bfd.js
+++ js/bfd.js
@@ -1,6 +1,10 @@
 $('#content').prepend('<a href="' + chrome.extension.getURL("options.html") + '">BFD - Options</a>');
 
+// Find all the ingredient rows.
 $('#main .container tbody tr:not([class])').each(function(){
+    // TODO: Change so you display an entire column of efficiencies at once.
+
+    // Get the cells which only contain nutrition data.
     var tds = $(this).find('td:not([class])');
 
     var td_to_int = function(td){
@@ -10,7 +14,7 @@ $('#main .container tbody tr:not([class])').each(function(){
     var calories = td_to_int(tds[0]); 
 
     var add_rollover = function(td, efficiency, value){
-        // TODO: Change the colours to a scale depending on efficiency
+        // TODO: Change the colours to scale depending on efficiency
         $(td).mouseenter(function(){
             $(td).css('color','red');
             $(td).html(efficiency);
@@ -25,6 +29,7 @@ $('#main .container tbody tr:not([class])').each(function(){
         var td = tds[i];
         var value = td_to_int(td);
         if(value !== 0){
+            // Round the efficiency to 2 decimal places.
             var efficiency = Math.round( (calories / value) * 100 ) / 100;
             add_rollover(td, efficiency, value);
         }
