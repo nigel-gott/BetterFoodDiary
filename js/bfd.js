c1ac82a80910520b3289973dc6ca4db0b04a4196
Nigel Gott <nigel.gott@gmail.com>
2012-11-21 16:35:41 +0000

    Added basic calorie / marco efficiency display to food diary.

--- js/bfd.js
+++ js/bfd.js
@@ -1,1 +1,20 @@
 $('#content').prepend('<a href="' + chrome.extension.getURL("options.html") + '">BFD - Options</a>');
+
+$('#main .container tbody tr:not([class])').each(function(){
+    var tds = $(this).find('td:not([class])');
+
+    var td_to_int = function(td){
+        return parseInt($(td).html()); 
+    }
+
+    var calories = td_to_int(tds[0]); 
+
+    for(var i = 1; i < tds.length; i++){
+        var td = tds[i];
+        var value = td_to_int(td);
+        if(value !== 0){
+            var efficiency = Math.round( (calories / value) * 100 ) / 100;
+            $(td).append(' - ' + efficiency);
+        }
+    }
+});
