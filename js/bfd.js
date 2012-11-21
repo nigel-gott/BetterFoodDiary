18569b3ed3f91b54021805d5de8151b83dff4799
Nigel Gott <nigel.gott@gmail.com>
2012-11-21 16:49:13 +0000

    Now shows calorie efficiency on rollover.

--- js/bfd.js
+++ js/bfd.js
@@ -9,12 +9,22 @@ $('#main .container tbody tr:not([class])').each(function(){
 
     var calories = td_to_int(tds[0]); 
 
+    var add_rollover = function(td, efficiency, value){
+        $(td).mouseenter(function(){
+            $(td).html(efficiency);
+        });
+        $(td).mouseleave(function(){
+            $(td).html(value);
+        });
+    }
+
     for(var i = 1; i < tds.length; i++){
         var td = tds[i];
         var value = td_to_int(td);
         if(value !== 0){
             var efficiency = Math.round( (calories / value) * 100 ) / 100;
-            $(td).append(' - ' + efficiency);
+            add_rollover(td, efficiency, value);
         }
     }
+
 });
