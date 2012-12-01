bc67fe508f3b550aad3c64170223684c63a4ed74
Nigel Gott <nigel.gott@gmail.com>
2012-12-01 10:44:35 +0000

    Removed background_diary_scraper, cleaned up unittests.html.

--- tests/scraper.js
+++
@@ -1,5 +0,0 @@
-describe("Background diary scraper tests", function() {
-    it("is defined", function() {
-        expect(bfd.scrape_and_store_meals).toBeDefined();
-    });
-});
---
+++ tests/scraper_tests.js
@@ -0,0 +1,5 @@
+describe("Scraper test suite", function() {
+    it("scrape_and_store_meals is defined", function() {
+        expect(bfd.scrape_and_store_meals).toBeDefined();
+    });
+});
