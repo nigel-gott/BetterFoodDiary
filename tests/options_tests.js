da110e42a86eaf849e3b0edc366e095945758829
Nigel Gott <nigel.gott@gmail.com>
2012-12-18 18:39:39 +0000

    Added basic integration test of the options page.

---
+++ tests/options_tests.js
@@ -0,0 +1,41 @@
+describe('Options page', function() {
+    // Integration testing of the options page by loading it in an iframe.
+
+    var $iframe;
+
+    beforeEach(function() {
+        $iframe = $('<iframe/>', {
+            src: chrome.extension.getURL("options.html"),
+        });
+        $('body').append($iframe);
+    });
+
+    afterEach(function() {
+        $iframe.remove();
+    });
+
+    it('scrapes data when scrape link is clicked.', function() {
+        var contents_before;
+        runs(function() {
+            $iframe.load(function() {
+                contents_before = $iframe.contents().find('#meals').html();
+                // Must click the actual DOM node, this click function is raw 
+                // JS and not the jquery click function.
+                // The jQuery click does not fire the click event properly on 
+                // A tags as we are trying to do here.
+                $iframe.contents().find('#scrape_meals_link')[0].click();
+            });
+        });
+
+        waits(10000);
+
+        runs(function() { 
+            var contents_after = $iframe.contents().find('#meals').html();
+            expect(contents_after).toNotBe(contents_before);
+            // if h2 tags are present then an error has occured.
+            expect(contents_after).toNotContain('h2');
+        });
+    });
+
+});
+
