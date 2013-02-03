6c13600533307cf79276a2ba3eeba3d1cc092dda
Nigel Gott <nigel.gott@gmail.com>
2013-02-03 14:12:39 +0000

    food_diary.js wont assign bfd to {} if undefined.
    No need to do this in a file which is not extending the bfd global
    object and is instead just using it.

--- js/cs/food_diary.js
+++ js/cs/food_diary.js
@@ -1,6 +1,4 @@
 // Content script for "http://www.myfitnesspal.com/food/diary/*"
 
-var bfd = bfd || {};
-
 var parser = new bfd.DiaryParser($('#main .container table'));
 parser.parse().get_total_nutrients();
