a587118c3bcddb0682d901525d587b1f56153066
Nigel Gott <nigel.gott@gmail.com>
2013-02-03 16:35:25 +0000

    Bound click on stats button to dummy function.

--- js/views.js
+++ js/views.js
@@ -2,10 +2,13 @@ var bfd = bfd || {};
 
 bfd.StatsView = Backbone.View.extend({
     tagName: 'td',
+    events: {
+        'click #stats_button' : 'toggle_stats_view'
+    },
     initialize: function(){
         var first_meal_header = $('.meal_header').first();
 
-        var stats_button = $('<a/>');
+        var stats_button = $('<a/>', {id : 'stats_button'} );
         var stats_image = $('<img/>', {
             src: chrome.extension.getURL('images/stats_icon.png')
         });
@@ -13,5 +16,7 @@ bfd.StatsView = Backbone.View.extend({
         stats_button.append(stats_image);
         this.$el.append(stats_button);
         first_meal_header.append(this.$el);
+    },
+    toggle_stats_view: function(){
     }
 });
