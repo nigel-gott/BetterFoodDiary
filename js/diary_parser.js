34a92eae4d57f4d0d1f5111e27f0432d4c9e8cbd
Nigel Gott <nigel.gott@gmail.com>
2013-02-09 20:34:18 +0000

    Implemented better Id generation for models.
    Now the ID isn't just a number, it is a combinition of one or more of
    the following:
    
    The date, the meal number, the ingredient number, the nutrient number.

--- js/diary_parser.js
+++ js/diary_parser.js
@@ -2,61 +2,57 @@ var bfd = bfd || {};
 
 bfd.DiaryEntryParser = function DiaryEntryParser(dom_parser, create_nutrient_views) {
 
-    var i = 1;
-
-    function id(){
-        i++;
-        return i;
-    }
-
     this.parse = function parse(){
         meal_headers = dom_parser.get_meal_headers(); 
         nutrient_names = dom_parser.get_nutrient_names(meal_headers);
         entry_date = dom_parser.get_entry_date();
 
+
         var entry = new bfd.DiaryEntry({
-            id: id(),
+            id: entry_date,
             date: entry_date,
             meals: []
         }); 
 
-        parse_collection(entry.get('meals'), meal_headers, parse_meal);
+        parse_collection(entry.get('meals'), meal_headers, parse_meal, entry_date);
 
         console.log(entry.toJSON());
         entry.save();
         return entry;
     }
 
-    function parse_collection(collection, element_list, parse_function){
+    function parse_collection(collection, element_list, parse_function, id_prefix){
         element_list.each(function(index, element){
-            var model = parse_function(index, element, collection);
+            var model = parse_function(index, element, id_prefix);
             collection.add(model);
         });
     }
 
-    function parse_meal(index, meal_header){
+    function parse_meal(index, meal_header, id_prefix){
         var ingredient_rows = dom_parser.get_ingredient_rows(meal_header); 
+        var id = id_prefix + '_meal' + index;
         var meal = new bfd.Meal({
-            id: id(),
+            id: id,
             name: dom_parser.get_meal_name(meal_header),
             ingredients: []
         });
 
-        parse_collection(meal.get('ingredients'), ingredient_rows, parse_ingredient)
+        parse_collection(meal.get('ingredients'), ingredient_rows, parse_ingredient, id);
 
         return meal;
     }
 
-    function parse_ingredient(index, ingredient_row){
+    function parse_ingredient(index, ingredient_row, id_prefix){
         var nutrient_cells = dom_parser.get_nutrient_cells(ingredient_row); 
 
+        var id = id_prefix + '_ingredient' + index;
         var ingredient = new bfd.Ingredient({
-            id: id(),
+            id: id,
             name: dom_parser.get_ingredient_name(ingredient_row),
             nutrients: []
         });
 
-        parse_collection(ingredient.get('nutrients'), nutrient_cells, parse_nutrient);
+        parse_collection(ingredient.get('nutrients'), nutrient_cells, parse_nutrient, id);
 
         // Optional function that can be passed in
         if(typeof create_nutrient_views === 'function'){
@@ -66,9 +62,9 @@ bfd.DiaryEntryParser = function DiaryEntryParser(dom_parser, create_nutrient_vie
         return ingredient;
     }
 
-    function parse_nutrient(index, nutrient_cell, nutrients){
+    function parse_nutrient(index, nutrient_cell, id_prefix){
             return new bfd.Nutrient({
-                id: id(),
+                id: id_prefix + '_nutrient' + index,
                 name: nutrient_names[index],
                 value: dom_parser.get_nutrient_value(nutrient_cell)
             });
@@ -83,7 +79,11 @@ bfd.FoodDiaryDomParser = function FoodDiaryDomParser(){
     };
 
     this.get_entry_date = function get_entry_date(){
-        return new Date();
+        var d = new Date($('#date_selector').val());
+        var curr_date = d.getDate();
+        var curr_month = d.getMonth() + 1; //Months are zero based
+        var curr_year = d.getFullYear();
+        return curr_date + "/" + curr_month + "/" + curr_year;
     };
 
     // Returns the names of the nutrients displayed in the food diary.
